b3915b971f91cb898da6c9cff0ee910fb321024a
Shwetank Chandra <chandreium@gmail.com>
2025-02-28 13:50:22 +0530

    complete the chatbot UI

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,37 +1,86 @@
+import { useEffect, useRef, useState } from "react"
 import ChatbotIcon from "./components/ChatbotIcon"
 import ChatForm from "./components/ChatForm"
+import ChatMessage from "./components/ChatMessage";
 
 const App = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+  const [showChatbot, setShowChatbot] = useState(false);
+  const chatBodyRef = useRef();
+
+  const generateBotResponse = async (history) => {
+    const updateHistory = (text, isError = false) => {
+      setChatHistory(prev => [...prev.filter(msg => msg.text !== "Thinking..."), { role: "model", text, isError }])
+    }
+
+
+    history = history.map(({ role, text }) => ({ role, parts: [{ text }] }));
+
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contents: history }),
+
+    }
+
+    try {
+      const response = await fetch(import.meta.env.VITE_API_URL, requestOptions);
+      const data = await response.json();
+      if (!response.ok) throw new Error(data.error.message || "Something went wrong!")
+
+      const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+
+      updateHistory(apiResponseText);
+    } catch (error) {
+      updateHistory(error.messsage, true);
+
+    }
+
+
+  }
+
+  useEffect(() => {
+    chatBodyRef.current.scrollTo({ top: chatBodyRef.current.scrollHeight, behavior: "smooth" })
+  }, [chatHistory]);
+
+
   return (
-    <div className="container">
+    <div className={`container ${showChatbot ? 'show-chatbot' : ""}`}>
+      <button onClick={() => setShowChatbot(prev => !prev)} id="chatbot-toggler">
+        <span class="material-symbols-rounded">mode_comment</span>
+        <span class="material-symbols-rounded">close</span>
+      </button>
+      
+
       <div className="chatbot-popup">
         <div className="chat-header">
           <div className="header-info">
-          <ChatbotIcon />
+            <ChatbotIcon />
             <h2 className="logo-text">Chatbot</h2>
           </div>
-          <button class="material-symbols-rounded">
+          <button onClick={() => setShowChatbot(prev => !prev)} class="material-symbols-rounded">
             keyboard_arrow_down
           </button>
         </div>
 
-        <div className="chat-body">
+        <div ref={chatBodyRef} className="chat-body">
           <div className="message bot-message">
-          <ChatbotIcon />
-          <p className="message-text">
-            Hey there 👋 <br /> How can I help you Today?
-          </p>
+            <ChatbotIcon />
+            <p className="message-text">
+              Hey there 👋 <br /> How can I help you Today?
+            </p>
 
           </div>
-          <div className="message user-message">
-            <p className="message-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odio placeat consequuntur reprehenderit alias expedita! Nemo omnis quo velit ipsam sequi? Officiis, eius rerum!
-            </p>
-          </div>
+
+          {chatHistory.map((chat, index) => (
+            <ChatMessage key={index} chat={chat} />
+          ))}
+
 
         </div>
         <div className="chat-footer">
-          <ChatForm />
+          <ChatForm chatHistory={chatHistory} setChatHistory={setChatHistory} generateBotResponse={generateBotResponse} />
         </div>
 
       </div>
--- chatbot/src/components/ChatForm.jsx
+++ chatbot/src/components/ChatForm.jsx
@@ -1,7 +1,25 @@
-const ChatForm = () => {
+import { useRef } from "react"
+
+const ChatForm = ({chatHistory, setChatHistory, generateBotResponse}) => {
+    const inputRef = useRef();
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const userMessage = inputRef.current.value.trim();
+        if(!userMessage) return;
+        inputRef.current.value = "";
+
+        setChatHistory(history => [...history, {role: 'user', text: userMessage}]);
+
+        setTimeout(() => setChatHistory(history => [...history, {role: 'model', text: 'Thinking...'}]), 600);
+
+        generateBotResponse([...chatHistory, {role: 'user', text: userMessage}]);
+    }
+
+
     return (
-        <form action="#" className="chat-form">
-            <input type="text" placeholder="Message..." className="message-input" required />
+        <form action="#" className="chat-form" onSubmit={handleFormSubmit}>
+            <input ref={inputRef} type="text" placeholder="Message..." className="message-input" required />
             <button class="material-symbols-rounded">
                 arrow_upward
             </button>
@@ -9,4 +27,4 @@ const ChatForm = () => {
     )
 }
 
-export default ChatForm
+export default ChatForm   
